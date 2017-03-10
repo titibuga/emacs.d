af5232d3233142d9d0664a3b7caac04528c1036a
Victor Portella <visportella@gmail.com>
2017-03-10 20:19:21 -0300

    Magit, Melpa, and some other key bindings

--- init.el
+++ init.el
@@ -1,10 +1,18 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes (quote (tango-dark)))
- '(inhibit-startup-screen t))
+ '(inhibit-startup-screen t)
+ '(package-selected-packages (quote (magit auctex))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -36,3 +44,29 @@
 ;;TODO: Fix this so it doesn't spit the output in emacs itself
 (global-set-key (kbd "C-c p")
   (lambda () (interactive )(shell-command "latexmk -pdf"))) 
+
+
+;; font size (from https://github.com/bbatsov/prelude)
+(global-set-key (kbd "C-+") 'text-scale-increase)
+(global-set-key (kbd "C--") 'text-scale-decrease)
+
+
+;; Disable C-x C-x to trigger region activation (from masteringemacs.org) 
+(defun exchange-point-and-mark-no-activate ()
+  "Identical to \\[exchange-point-and-mark] but will not activate the region."
+  (interactive)
+  (exchange-point-and-mark)
+  (deactivate-mark nil))
+(define-key global-map [remap exchange-point-and-mark] 'exchange-point-and-mark-no-activate)
+
+;; Regex fix
+(require 're-builder)
+(setq reb-re-syntax 'string)
+
+;; Melpa
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+
+;; Magit
+(global-set-key (kbd "C-x g") 'magit-status)
