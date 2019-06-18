66c97d2249992c80ce956fdb55a788f07a15dbd2
Victor Portella <visportella@gmail.com>
2019-06-18 18:43:28 -0300

    Add python support and organize package list

--- init.el
+++ init.el
@@ -16,13 +16,27 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- ;; '(ansi-color-names-vector
- ;;   ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#e090d7" "#8cc4ff" "#eeeeec"])
- ;; '(custom-enabled-themes nil)
  '(inhibit-startup-screen t)
  '(package-selected-packages
    (quote
-    (ess company-auctex vue-mode ido-ubiquitous ido-vertical-mode flx-ido flymake-ruby smartparens company avy auctex-latexmk typescript-mode magit material-theme rainbow-delimiters))))
+    (elpy ;;
+     ess ;;
+     company-auctex ;;
+     vue-mode ;;
+     ido-ubiquitous ;;
+     ido-vertical-mode ;;
+     flx-ido ;;
+     flymake-ruby ;;
+     smartparens ;;
+     company ;;
+     avy ;;
+     auctex-latexmk ;;
+     typescript-mode ;;
+     magit ;;
+     material-theme ;;
+     rainbow-delimiters ;;
+     flycheck ;;
+     ))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -37,6 +51,15 @@
 ;; Remove Menubar
 (menu-bar-mode 0)
 (tool-bar-mode 0)
+
+;; Activate elpy for python files and change auto-completion engine
+;; TODO: Load elpy lazily
+(elpy-enable)
+(when (require 'flycheck nil t)
+  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+  (add-hook 'elpy-mode-hook 'flycheck-mode))
+
+
 
 
 ;; Auto-installl packages in the package list
